06f3da0814398fdf95139cd8583d9e1efdf2be20
grace <gracechongbl@gmail.com>
2021-04-16 00:35:59 +0800

    postsController, migration, updated web.php

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -7,14 +7,14 @@
             <div class="row">
             <div class="col-8">              
                 <div class="list-group" id="list-tab" role="tablist">
-                    <a class="list-group-item list-group-item-action active" id="list-newhelper-list" data-toggle="list" href="#list-newhelper" role="tab" aria-controls="newhelper">New helper</a>
-                    <a class="list-group-item list-group-item-action" id="list-transferhelper-list" data-toggle="list" href="#list-transfer" role="tab" aria-controls="transferhelper">Transfer helper</a>
-                    <a class="list-group-item list-group-item-action" id="list-renewals-list" data-toggle="list" href="#list-renewals" role="tab" aria-controls="renewals">Renewals of work permits and other passes</a>
-                    <a class="list-group-item list-group-item-action" id="list-homeleave-list" data-toggle="list" href="#list-homeleave" role="tab" aria-controls="homeleave">Home Leave Documentation</a>
-                    <a class="list-group-item list-group-item-action" id="list-passport-list" data-toggle="list" href="#list-passport" role="tab" aria-controls="passport">Passport Renewal</a>
-                    <a class="list-group-item list-group-item-action" id="list-applicationpr-list" data-toggle="list" href="#list-applicationpr" role="tab" aria-controls="applicationpr">Application for PR</a>
+                    <h4><a class="list-group-item list-group-item-action active" id="list-newhelper-list" data-toggle="list" href="#list-newhelper" role="tab" aria-controls="newhelper">New helper</a></h4>
+                    <h4><a class="list-group-item list-group-item-action" id="list-transferhelper-list" data-toggle="list" href="#list-transfer" role="tab" aria-controls="transferhelper">Transfer helper</a></h4>
+                    <h4><a class="list-group-item list-group-item-action" id="list-renewals-list" data-toggle="list" href="#list-renewals" role="tab" aria-controls="renewals">Renewals of work permits and other passes</a></h4>
+                    <h4><a class="list-group-item list-group-item-action" id="list-homeleave-list" data-toggle="list" href="#list-homeleave" role="tab" aria-controls="homeleave">Home Leave Documentation</a></h4>
+                    <h4><a class="list-group-item list-group-item-action" id="list-passport-list" data-toggle="list" href="#list-passport" role="tab" aria-controls="passport">Passport Renewal</a></h4>
+                    <h4><a class="list-group-item list-group-item-action" id="list-applicationpr-list" data-toggle="list" href="#list-applicationpr" role="tab" aria-controls="applicationpr">Application for PR</a></h4>
                 </div>
-        </div>
+            </div>
         </div>
             <div class="col-10">
                     <div class="tab-content" id="nav-tabContent">
